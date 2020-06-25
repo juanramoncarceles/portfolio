607c69d7549870c23c3daf3b16faa3e89eb92599
Ramon Carceles <juanramoncarceles@gmail.com>
2020-06-25 20:00:05 +0200

    Added page title in the webpack config.

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,11 @@ module.exports = {
   lintOnSave: false,
   configureWebpack: {
     devtool: "source-map"
+  },
+  chainWebpack: config => {
+    config.plugin("html").tap(args => {
+      args[0].title = "Ramón Cárceles | Front-end developer";
+      return args;
+    });
   }
 };
