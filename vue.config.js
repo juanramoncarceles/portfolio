8a49fdd91a15eea7dd0b2037767adc59166630dd
Ramon Carceles <juanramoncarceles@gmail.com>
2020-07-06 21:29:30 +0200

    Added publicPath

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,7 @@ module.exports = {
   configureWebpack: {
     devtool: "source-map"
   },
+  publicPath: process.env.NODE_ENV === "production" ? "/portfolio/" : "/",
   chainWebpack: config => {
     config.plugin("html").tap(args => {
       args[0].title = "Ramón Cárceles | Web developer";
